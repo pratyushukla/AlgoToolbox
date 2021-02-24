289ceceeeb688824d89b985fdc5af58905cb0096
Pratyush Shukla <pratyushukla1999@gmail.com>
2021-02-24 11:38:46 +0530

    This is my first commit using VScode.

---
+++ week1/twoDigitSum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b) {
+    return a + b;
+}
+int main (void) {
+    int a, b;
+    cin >> a >> b;
+    cout << sum(a, b) << endl;
+    return 0;
+}
