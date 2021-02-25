d770e81b3d701fbb19ff3f7df316dee54958dcd8
Pratyush Shukla <pratyushukla1999@gmail.com>
2021-02-26 00:05:29 +0530

    Changed fibonacci program to meet problem statement requirements.

--- week2/Fibonacci.cpp
+++ week2/Fibonacci.cpp
@@ -1,21 +1,18 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
+long long fibonacci(int n) {
+    vector<long long> fibonacci(n + 1);
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (int i = 0; i < n; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    return fibonacci[n]; 
+}
+
 int main(void) {
     int n;
-    cin >> n; //Number of Fibonacci numbers to be printed
-    vector <int> array(n);
-    for (int i = 0; i < n; i++) {
-        array[i] = 0;
-    }
-    array[1] = 1;
-    printf("\n");
-    printf("\t\t--------------Fibonacci Numbers--------------\n");
-    printf("1st Fibonacci number = 0\n");
-    printf("2nd Fibonacci number = 1\n");
-    for (int i = 2; i < n; i++) {
-        array[i] = array[i - 1] + array[i - 2];
-        printf("%dth Fibonacci number = %d\n", i, array[i]);
-    }
-    return 0;
+    cin >> n;
+    cout << fibonacci(n) << endl;
 }
