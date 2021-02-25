f377c1cd8524406a0b9681a8454cbed2e0c09330
Pratyush Shukla <pratyushukla1999@gmail.com>
2021-02-26 00:14:36 +0530

    Removed bugs

--- week2/Fibonacci.cpp
+++ week2/Fibonacci.cpp
@@ -5,7 +5,7 @@ long long fibonacci(int n) {
     vector<long long> fibonacci(n + 1);
     fibonacci[0] = 0;
     fibonacci[1] = 1;
-    for (int i = 0; i < n; i++) {
+    for (int i = 2; i <= n; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
     }
     return fibonacci[n]; 
